20c28fd2807d03c38b12e87cfca59d08fa61e394
shubham <ss.guwahati1996@gmail.com>
2024-10-02 17:28:27 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Total Cost Calculator with Tax and Shipping
+
+This project calculates the total cost of products, incorporating tax rates and shipping fees. It leverages partial application to simplify function calls for calculating costs. The application reads product data from a JSON file and renders it dynamically on a web page.
+
+## Table of Contents
+
+- [Features](#features)
+- [Usage](#usage)
+
+## Features
+
+- Calculate total costs based on different tax rates.
+- Support for default shipping rates for specific product categories.
+- Dynamic rendering of product information on a web page.
+
+## Usage
+
+- Ensure you have a modern web browser.
+- Open the index.html file in your browser to view the product list and total cost calculations.
+- You can modify the data.json file to add or change products and see how the total cost calculation updates.
---
+++ src/js/index.js
@@ -0,0 +1,47 @@
+import data from './data.json';
+import Row from './Row';
+const productsDiv = document.querySelector('#products');
+
+const renderRows = function (items) {
+  const domNodes = items
+    .map(({ product, cost, totalCost = 'N/A' }) =>
+      Row(product, cost, totalCost)
+    )
+    .join('');
+
+  productsDiv.innerHTML = domNodes;
+};
+
+const totalCost = function (tax, shipping, cost) {
+  return (Number(tax) / 100) * Number(cost) + Number(cost) + Number(shipping);
+};
+
+// smartphones and catfood are taxed at 12%
+// televisions are taxed at 18%
+// Default shipping rates (if absent in dataset): Pet food (2.5), Smartphones (5), Televisions (15)
+
+const tax = {
+  tax12 : totalCost.bind(null, 12),
+  tax18 : totalCost.bind(null, 18),
+};
+
+const shipping = {
+  petfood : tax.tax12.bind(null, 2.5),
+  smartphones : tax.tax12.bind(null, 5),
+  televisions : tax.tax18.bind(null, 15),
+};
+
+(function() {
+  const revised = data.map(item => { 
+    console.log(shipping[item.category](item.cost));
+    return {
+    ...item,
+    totalCost : !item.shipping ? 
+    shipping[item.category](item.cost) :
+     ['petfood','smartphones'].includes(item.category) ? tax.tax12(item.shipping, item.cost) :
+     tax.tax18(item.shipping, item.cost),
+  };
+});
+
+renderRows(revised);
+})();
