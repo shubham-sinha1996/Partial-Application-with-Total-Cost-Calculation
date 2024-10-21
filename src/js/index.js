25c90d33ba1a612f6b3c78d27ed318b1929bf193
shubham <ss.guwahati1996@gmail.com>
2024-10-21 18:45:07 +0530

    Update to IIFE

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,13 @@
-import data from './data.json';
-import Row from './Row';
-const productsDiv = document.querySelector('#products');
+import data from "./data.json";
+import Row from "./Row";
+const productsDiv = document.querySelector("#products");
 
 const renderRows = function (items) {
   const domNodes = items
-    .map(({ product, cost, totalCost = 'N/A' }) =>
+    .map(({ product, cost, totalCost = "N/A" }) =>
       Row(product, cost, totalCost)
     )
-    .join('');
+    .join("");
 
   productsDiv.innerHTML = domNodes;
 };
@@ -21,27 +21,27 @@ const totalCost = function (tax, shipping, cost) {
 // Default shipping rates (if absent in dataset): Pet food (2.5), Smartphones (5), Televisions (15)
 
 const tax = {
-  tax12 : totalCost.bind(null, 12),
-  tax18 : totalCost.bind(null, 18),
+  tax12: totalCost.bind(null, 12),
+  tax18: totalCost.bind(null, 18),
 };
 
 const shipping = {
-  petfood : tax.tax12.bind(null, 2.5),
-  smartphones : tax.tax12.bind(null, 5),
-  televisions : tax.tax18.bind(null, 15),
+  petfood: tax.tax12.bind(null, 2.5),
+  smartphones: tax.tax12.bind(null, 5),
+  televisions: tax.tax18.bind(null, 15),
 };
 
-(function() {
-  const revised = data.map(item => { 
-    console.log(shipping[item.category](item.cost));
+(function () {
+  const revised = data.map((item) => {
     return {
-    ...item,
-    totalCost : !item.shipping ? 
-    shipping[item.category](item.cost) :
-     ['petfood','smartphones'].includes(item.category) ? tax.tax12(item.shipping, item.cost) :
-     tax.tax18(item.shipping, item.cost),
-  };
-});
+      ...item,
+      totalCost: !item.shipping
+        ? shipping[item.category](item.cost)
+        : ["petfood", "smartphones"].includes(item.category)
+        ? tax.tax12(item.shipping, item.cost)
+        : tax.tax18(item.shipping, item.cost),
+    };
+  });
 
-renderRows(revised);
+  renderRows(revised);
 })();
